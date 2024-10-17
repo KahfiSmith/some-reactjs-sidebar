98d8f025f62b512b2bb6149639392cb0d5de8fe5
KahfiSmith <alkahfii2018@gmail.com>
2024-10-17 12:36:18 +0700

    initial commit

---
+++ src/components/atoms/label.tsx
@@ -0,0 +1,12 @@
+import { LabelProps } from "@/types";
+
+export default function Label({ htmlFor, children, className }: LabelProps) {
+  return (
+    <label
+      htmlFor={htmlFor}
+      className={`block md:text-sm lg:text-base font-medium text-gray-500 mb-1 ${className}`}
+    >
+      {children}
+    </label>
+  );
+}
